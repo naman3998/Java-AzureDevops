e745bb316ae71b62ca0f790a0dbf4360cbcfea63
namanahuja <naman3998@users.noreply.github.com>
2022-05-21 14:15:50 +0530

    first

---
+++ springboot-welcome/src/main/java/com/java/springbootwelcome/SpringbootWelcomeApplication.java
@@ -0,0 +1,13 @@
+package com.java.springbootwelcome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWelcomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWelcomeApplication.class, args);
+	}
+
+}
---
+++ springboot-welcome/src/main/java/com/java/springbootwelcome/WelcomeController.java
@@ -0,0 +1,15 @@
+package com.java.springbootwelcome;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+	@RestController
+	public class WelcomeController {
+	    
+	    @RequestMapping("/")
+	    public String index() {
+	        return "Hello from Spring Boot!";
+	    }   
+	}
+
+
